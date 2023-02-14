e1e0ac576d138bb0e834b48e0c2f31690b5b1c84
leoguain <108464746+leoguain@users.noreply.github.com>
2023-02-14 18:04:10 -0300

    AdSense Add

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import { theme } from "../styles/theme/index";
 import TagManager from "react-gtm-module";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }: AppProps) {
   useEffect(() => {
@@ -11,6 +12,17 @@ export default function App({ Component, pageProps }: AppProps) {
 
   return (
     <ChakraProvider theme={theme}>
+      <Script
+        id="Adsense-id"
+        data-ad-client="ca-pub-6520138402418007"
+        async
+        strategy="afterInteractive"
+        onError={(e) => {
+          console.error("Script failed to load", e);
+        }}
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+      />
+
       <Component {...pageProps} />
     </ChakraProvider>
   );
