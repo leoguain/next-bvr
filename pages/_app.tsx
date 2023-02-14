cd8d6526451ce16aa0732982d0429abc667eb26b
leoguain <108464746+leoguain@users.noreply.github.com>
2023-02-14 14:35:31 -0300

    GTM insertion

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
+import React, { useEffect } from "react";
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import { theme } from "../styles/theme/index";
+import TagManager from "react-gtm-module";
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    TagManager.initialize({ gtmId: "GTM-W7GVFVT" });
+  }, []);
+
   return (
     <ChakraProvider theme={theme}>
       <Component {...pageProps} />
